6362467118d07f0727af01d02fe983f7a846b94b
Papafrick93 <112095410+Papafrick93@users.noreply.github.com>
2024-05-25 12:57:35 +0300

    Complete js exercises 01 to 07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, numOfRepeats) {
+    let resultString = ''
+    
+    if (numOfRepeats < 0) {
+        return resultString = 'ERROR';
+    }
 
+    for (let i = 0; i < numOfRepeats; i++) {
+        resultString += string;
+    }
+    
+    return resultString ;
 };
 
 // Do not edit below this line
